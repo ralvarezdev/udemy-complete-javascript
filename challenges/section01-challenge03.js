52ae7b413b9834117cdd9c8bdce105aea653d829
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2023-10-16 20:11:14 -0400

    feat: Challenge 3 and 4 of JS Fundamentals Done

---
+++ challenges/section01-challenge03.js
@@ -0,0 +1,15 @@
+let scoreDolphins = 96 + 108 + 89;
+let scoreKoalas = 88 + 91 + 110;
+
+if (scoreDolphins > scoreKoalas)
+{
+  console.log("Dolphins win the trophy");
+}
+else if (scoreDolphins < scoreKoalas)
+{
+  console.log("Koalas win the trophy");
+}
+else
+{
+  "Both win the trophy";
+}
