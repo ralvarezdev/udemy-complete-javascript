627fb6b2d85d9fb01df10588ee6d6ac5b74a4eb0
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2023-12-19 07:24:35 -0400

    feat: Coding Challenge 4 of DS and Modern Operators Done

--- challenges/section09-challenge.js
+++ challenges/section09-challenge.js
@@ -205,3 +205,68 @@ console.log(`An event happened, on average, every ${ 90 / gameEvents.size } minu
 
 for (let [key, value] of gameEvents)
   console.log(`[${ key <= 45 ? 'FIRST' : 'SECOND' } HALF] ${ key }: ${ value }`);
+
+// Coding Challenge 4
+
+/*
+Write a program that receives a list of variable names written in underscore_case
+and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to
+insert the elements), and conversion will happen when the button is pressed.
+Test data (pasted to textarea, including spaces):
+underscore_case
+first_name
+Some_Variable
+calculate_AGE
+delayed_departure
+Should produce this output (5 separate console.log outputs):
+underscoreCase ✅
+firstName ✅✅
+someVariable ✅✅✅
+calculateAge ✅✅✅✅
+delayedDeparture ✅✅✅✅✅
+
+Hints:
+§ Remember which character defines a new line in the textarea 😉
+§ The solution only needs to work for a variable made out of 2 words, like a_b
+§ Start without worrying about the ✅. Tackle that only after you have the variable
+name conversion working 😉
+§ This challenge is difficult on purpose, so start watching the solution in case
+you're stuck. Then pause and continue!
+Afterwards, test with your own test data!
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+
+const textArea = document.querySelector('textarea');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () =>
+{
+  const params = textArea.value.split('\n');
+  const modParams = [];
+
+  for (let param of params)
+  {
+    param = param.toLowerCase();
+
+    const words = param.split('_');
+    const modWords = [];
+
+    modWords.push(words[0]);
+
+    for (let i = 1; i < words.length; i++)
+      modWords.push(words[i][0].toUpperCase() + words[i].slice(1));
+
+    modParams.push(modWords.join(''));
+  }
+
+  let msg = '';
+  for (let i = 0; i < modParams.length; i++)
+    msg = msg.concat(`${ modParams[i] } ${ '✅'.repeat(i + 1) }\n`);
+
+  console.log(msg);
+});
