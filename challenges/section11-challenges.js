7f2c86c68eb1fd6ac15a4874f2dc55837488a8be
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2023-12-20 18:20:10 -0400

    feat: Coding Challenge 2 of Arrays Section Done

--- challenges/section11-challenges.js
+++ challenges/section11-challenges.js
@@ -53,3 +53,41 @@ checkDogs(dogsJulia1, dogsKate1);
 
 console.log('----- TEST 2 -----');
 checkDogs(dogsJulia2, dogsKate2);
+
+// Coding Challenge 2
+
+/*
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's
+ages ('ages'), and does the following things in order:
+1. Calculate the dog age in human years using the following formula: if the dog is
+<= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old,
+humanAge = 16 + dogAge * 4
+2. Exclude all dogs that are less than 18 human years old (which is the same as
+keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know
+from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+Test data:
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+*/
+
+const dogsAge1 = [5, 2, 4, 1, 15, 8, 3];
+const dogsAge2 = [16, 6, 10, 5, 6, 1, 4];
+
+const calcAverageHumanAge = ages =>
+{
+  const humanYears = ages.map(curr => (curr <= 2) ? curr * 2 : 16 + curr * 4);
+  const humanYearsFiltered = humanYears.filter(curr => curr >= 18);
+  console.log(humanYearsFiltered);
+
+  let avr = humanYearsFiltered.reduce((acc, curr) => acc += curr, 0);
+  avr /= humanYearsFiltered.length;
+  console.log(`Average of Human Years: ${ avr }`);
+};
+
+console.log('----- TEST 1 -----');
+calcAverageHumanAge(dogsAge1);
+
+console.log('----- TEST 2 -----');
+calcAverageHumanAge(dogsAge2);
