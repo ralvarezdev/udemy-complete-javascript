510c8e1c2ad73f2354038e54b767486cb827d567
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2023-12-24 21:41:06 -0400

    feat: Coding Challenge 2 of OOP Section Done

--- challenges/section14-challenges.js
+++ challenges/section14-challenges.js
@@ -52,3 +52,77 @@ for (let j = 0; j < 3; j++)
 
 for (let k = 0; k < 4; k++)
   car1.brake();
+
+// Coding Challenge 2
+
+/*
+1. Re-create Challenge #1, but this time using an ES6 class (call it 'CarCl')
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide
+by 1.6)
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but
+converts it to km/h before storing the value, by multiplying the input by 1.6)
+4. Create a new car and experiment with the 'accelerate' and 'brake'
+methods, and with the getter and setter.
+Test data:
+§ Data car 1: 'Ford' going at 120 km/h
+*/
+
+class CarCl
+{
+  constructor (make, speed)
+  {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  get speedUs ()
+  {
+    return this.speed / 1.6;
+  }
+
+  set speedUs (miPerHour)
+  {
+    this.speed = miPerHour * 1.6;
+  }
+
+  logSpeed ()
+  {
+    console.log(`${ this.make } going at ${ this.speed } km/h`);
+  }
+
+  logSpeedUS ()
+  {
+    console.log(`${ this.make } going at ${ this.speedUs } mi/h`);
+  }
+
+  accelerate ()
+  {
+    this.speed += 10;
+    this.logSpeed();
+  }
+
+  brake ()
+  {
+    this.speed -= 5;
+    this.logSpeed();
+  }
+}
+
+const car3 = new CarCl('Ford', 120);
+
+car3.logSpeed();
+car3.logSpeedUS();
+
+for (let j = 0; j < 3; j++)
+  car3.accelerate();
+
+car3.logSpeedUS();
+
+for (let k = 0; k < 4; k++)
+  car3.brake();
+
+car3.logSpeedUS();
+
+car3.speedUs = 200;
+car3.logSpeedUS();
+car3.logSpeed();
