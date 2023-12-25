a4e4daa68afbb5ef76292e3b72ec96d9b19fb20f
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2023-12-24 20:33:45 -0400

    feat: Coding Challenge 1 of OOP Section Done

---
+++ challenges/section14-challenges.js
@@ -0,0 +1,54 @@
+'use strict';
+
+// Coding Challenge 1
+
+/*
+Your tasks:
+1. Use a constructor function to implement a 'Car'.A car has a 'make' and a;
+'speed' property.The 'speed' property is the current speed of the car in
+  km / h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10,
+and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log
+the new speed to the console;
+4. Create 2 'Car' objects and experiment with calling 'accelerate' and;
+'brake' multiple times on each of them
+Test data:
+§ Data car 1: 'BMW' going at 120 km / h
+§ Data car 2: 'Mercedes' going at 95 km / h;
+*/
+
+const Car = function (make, speed)
+{
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.logSpeed = function ()
+{
+  console.log(`${ this.make } going at ${ this.speed } km/h`);
+};
+
+Car.prototype.accelerate = function ()
+{
+  this.speed += 10;
+  this.logSpeed();
+};
+
+Car.prototype.brake = function ()
+{
+  this.speed -= 5;
+  this.logSpeed();
+};
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+
+for (let i = 0; i < 5; i++)
+  car1.accelerate();
+
+for (let j = 0; j < 3; j++)
+  car2.accelerate();
+
+for (let k = 0; k < 4; k++)
+  car1.brake();
